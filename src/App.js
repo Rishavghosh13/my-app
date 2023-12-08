4eb6d71bba34409c6b69aea947f5575da1b8ff53
Rishavghosh13 <153303694+Rishavghosh13@users.noreply.github.com>
2023-12-08 21:31:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+// import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import './App.css';
+import Textform from './components/Textform';
+import Alert from './components/Alert'
+import About from './components/About';
+import { useState } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+function App() {
+  const[change,setChange]=useState('primary')
+  const [val,setVal]=useState(1)
+  const handleR=()=>{
+    setVal(2)
+    setChange('danger')
+    if(mode==='dark')
+   { document.body.style.backgroundColor = '#851f0d'
+   
+  }
+}
+const handleY=()=>{
+  setVal(3)
+  setChange('warning')
+  if(mode==='dark')
+  {document.body.style.backgroundColor = '#949025'
+    }
+  
+}
+const handleB=()=>{
+  setVal(1)
+  setChange('primary')
+  if(mode==='dark')
+  {document.body.style.backgroundColor = '#094682'
+   }
+}
+const handleG=()=>{
+  setVal(4)
+  setChange('success')
+  if(mode==='dark')
+ {document.body.style.backgroundColor = '#046b1b'
+ }
+}
+  const[mode,setMode]=useState('light')
+  const toggleMode=()=>{
+    if(mode==='dark')
+    {setMode('light')
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = 'black'
+    showAlert('Dark Mode has been enabled',"success")
+    }
+    else
+    {setMode('dark')
+    if(val===1)
+    {document.body.style.backgroundColor = '#094682'
+    setChange('primary')}
+    else if(val===2)
+    {document.body.style.backgroundColor = '#851f0d'
+    setChange('danger')}
+    else if(val===3)
+    {document.body.style.backgroundColor = '#949025'
+    setChange('warning')}
+    else if(val===4)
+    {document.body.style.backgroundColor = '#046b1b'
+    setChange('success')}
+    document.body.style.color = 'white'
+    showAlert('Dark Mode has been disabled',"success")
+    }
+  }
+
+  const[alert,setAlert]=useState(null)
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  return (
+<>
+<Navbar title="Textutils" abouttext="About" mode={mode} toggleMode={toggleMode} change={val} handleB={handleB} handleG={handleG} handleR={handleR} handleY={handleY} />
+<Alert alert={alert}/>
+<Router>
+<div className="container">
+<Routes>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/" element={<Textform showAlert={showAlert} heading="Enter your text to analyze" mode={mode} change={change}/> }>
+            </Route>
+          </Routes>
+        
+</div>
+</Router>
+{/* <div className="container my=3">
+  <About/>
+  </div> */}
+</>
+  );
+}
+
+export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,80 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+    const handleUpClick=()=>{
+        console.log("Uppercase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to Uppercase","success")
+        
+    }
+    const handleOnChange=(event)=>{
+        console.log("On click");
+        setText(event.target.value)
+    }
+
+    const handleLwClick=()=>{
+      let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to Lowercase","success")
+    }
+    const handleSpeak=()=>{
+          let msg = new SpeechSynthesisUtterance();
+      msg.text = text;
+      window.speechSynthesis.speak(msg);
+      props.showAlert("is Speaking","success")
+
+    }
+    const handleClear=()=>
+   {
+    setText('')
+    props.showAlert("is Clear","success")
+   } 
+
+   const copyText = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Text is copied","success")
+}
+const clearSpace = () => {
+  let newT = text.split(/[ ]+/);
+  setText(newT.join(" "));
+  props.showAlert("Extra Space is cleared","success")
+}
+const cnt=()=>{
+  let newT = text.split(/[ ]+/);
+  newT= newT.join(" ")
+  const v=newT.split(" ").length
+  return v
+  
+
+}
+
+    const [text, setText] = useState('')
+ 
+
+  return (
+    <>
+    <div className='container'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3" >
+        <textarea style={{ backgroundColor: props.mode === 'light' ? 'white' : '#080808' , color:props.mode === 'light' ? 'black': 'white'}} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+
+
+        </div>
+        <button className={`btn btn-${props.change} border mx-2`} onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className={`btn btn-${props.change} border mx-2`} onClick={handleLwClick}>Convert to LowerCase</button>
+        <button className={`btn btn-${props.change} border mx-2`} onClick={handleSpeak}>Speak</button>
+        <button className={`btn btn-${props.change} border mx-2`} onClick={handleClear}>Clear</button>
+        <button className={`btn btn-${props.change} border mx-2`} onClick={copyText}>Copy</button>
+        <button className={`btn btn-${props.change} border mx-2`} onClick={clearSpace}>Clear Extra Space</button>
+        <div className="input-group mb-3">
+</div>
+</div>
+    <div className="container my-2">
+      <h1> Your text summary </h1>
+      <p>{text===""?0:text.charAt(text.length-1)===" "?cnt()-1:cnt()} words , {text.length} characters</p>
+      <p>{((text.length)*0.0016).toFixed(2)} Minutes read</p>
+    </div>
+    </>
+  )
+}
